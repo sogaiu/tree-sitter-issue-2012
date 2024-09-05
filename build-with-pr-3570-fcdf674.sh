ac4ca88401d9357b579fb7ea82943efce3f729dc
sogaiu <983021772@users.noreply.github.com>
2024-09-05 17:38:27 +0900

    Try fcdf674

---
+++ build-with-pr-3570-fcdf674.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+TS_DIR=tree-sitter-pr-3570-fcdf674
+
+cd "${TS_DIR}" && \
+  make clean && \
+  cd .. || exit && \
+  gcc \
+    -Itree-sitter/lib/include -Itree-sitter/lib/src tree-sitter/lib/src/lib.c \
+    -Itree-sitter-clojure/src tree-sitter-clojure/src/parser.c \
+    -o main-with-pr-3570-fcdf674 \
+    main.c
+
